0907a65730764690c33babbe165a997682886558
plaurion1989 <plaurion@protonmail.com>
2021-05-03 19:50:51 -0700

    third problem complete

--- starterCode/app.js
+++ starterCode/app.js
@@ -6,11 +6,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b, c) { //eslint-disable-line
   let sumOfNumbers = a + b;
-// returning an array
-// [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.']
-  let myArray = [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.']
+  let sumWithC = a + b + c;
+  let myArray = [sumOfNumbers, 'The sum of ' +a+' and ' +b+' is ' + sumOfNumbers +'.', sumWithC]
   return myArray;
 }
 
@@ -29,9 +28,20 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b, c) { //eslint-disable-line
 
-}
+    let productOfNumbers = a * b;
+    let productWithC = a * b * c; 
+    let myArray = [productOfNumbers, 'The product of ' +a+ ' and ' +b+' is ' +productOfNumbers + '.', productWithC];
+    return myArray;
+  }
+  
+  
+  
+  // Here is the test for multiply(); uncomment it to run it
+  // testMultiply(5,9);
+  testMultiply(5,9);
+
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
@@ -49,10 +59,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
   // sum, the function you wroote above, will do the math for you!
+  let productResults = multiply(a,b,c);
+  let getProduct = productResults[2];
+  let sumResults = sum(a,b,c);
+  let getSum = sumResults[2];
+  let problemThreeArray = [getSum, getProduct, a+ ' and ' +b+ ' and ' +c+ ' sum to ' + getSum + '.' , 'The product of ' +a+ ' and ' +b+ ' and ' +c+ ' is ' + getProduct + '.' ]
+  return problemThreeArray;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
